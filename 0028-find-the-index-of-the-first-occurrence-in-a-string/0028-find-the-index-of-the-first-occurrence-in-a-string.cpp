46cd7e6b01b274215555a82f268cc75cc1b669fa
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-03-13 11:54:29 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (34.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
+   void lpsfind(vector<int>&lps,string s) {
+	    // Your code goes here
+
+	    int prefix=0, suffix=1;
+	    while(suffix<s.size())
+	    {
+	        if(s[prefix]==s[suffix])
+	        {
+	            lps[suffix]=prefix+1;
+	            prefix++, suffix++;
+	        }
+	        else
+	        {
+	            if(prefix==0)
+	            {   lps[suffix]=0;
+	                suffix++;
+	            }
+	            else
+	            {
+	                prefix=lps[prefix-1];//crux
+	            }
+	        }
+	    }
+   }
     int strStr(string haystack, string needle) {
-        // Get the lengths of the haystack and needle strings
-        int n = haystack.size(), m = needle.size();
-        
-        // Loop through the haystack string
-        for (int i = 0; i <= n - m; i++) {
-            // Initialize variables to track positions in haystack and needle
-            int first = i, second = 0;
-            
-            // Check if substring starting from index 'i' in haystack matches needle
-            while (second < m) {
-                // If characters don't match, break out of the loop
-                if (haystack[first] != needle[second])
-                    break;
-                else
-                    // Move to the next character in both haystack and needle
-                    first++, second++;
+        vector<int>lps(needle.size(),0);
+        lpsfind(lps,needle);
+        int first=0, second=0;
+        while(second<needle.size()&&first<haystack.size())
+        {
+            if(needle[second]==haystack[first])
+            {
+                second++,first++;
             }
-            
-            // If we've matched the entire needle, return the starting index
-            if (second == m)
-                return i;
+            else
+            {
+                if(second==0)
+                {
+                    first++;
+                }
+                else
+                {
+                    second=lps[second-1];
+                }
+            }
         }
-        
-        // If needle is not found in haystack, return -1
-        return -1;
+        if(second==needle.size())
+            return first-second;
+        else
+            return -1;
     }
 };
-
