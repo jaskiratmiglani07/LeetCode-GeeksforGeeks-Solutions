b52e674287f99d7498c83dd9e4aa60482a895fb8
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-04-24 21:40:39 +0530

    Time: 4 ms (42.82%), Space: 8.6 MB (97.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-void permut(vector<int>&arr, vector<vector<int> >&ans, vector<int> &temp, vector<bool> &visited )
-{   
-    //base condition
-    if(visited.size()==temp.size())
+void permut(vector<int>&arr, vector<vector<int> >&ans, int index)
+{
+    if(index==arr.size())
     {
-        ans.push_back(temp);
+        ans.push_back(arr);
         return;
     }
-    for( int i=0; i<visited.size(); i++)
+    for(int i=index; i<arr.size(); i++)
     {
-        if(visited[i]==0)
-        {
-            visited[i]=1;
-            temp.push_back(arr[i]);
-            permut(arr, ans, temp, visited);
-            visited[i]=0;
-            temp.pop_back();
-        }
-
+        swap(arr[i],arr[index]);
+        permut(arr,ans, index+1);//once the function call is over, put it back to its original place
+        swap(arr[i],arr[index]);
     }
 }  
     vector<vector<int>> permute(vector<int>& nums) {
          vector<vector<int> >ans;
-         vector<int> temp;
-         vector<bool> visited(nums.size(),0);
-         permut(nums,ans,temp,visited);
+int index=0;
+         permut(nums,ans,index);
          return ans;
     }
 };
