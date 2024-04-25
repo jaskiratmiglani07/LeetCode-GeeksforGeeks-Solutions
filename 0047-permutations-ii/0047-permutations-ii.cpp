d8dbf43d9d3d475eac3212839f05a3a9679f7826
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-04-25 10:39:37 +0530

    Time: 3 ms (90.01%), Space: 11.8 MB (50.68%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void permut(vector<int>&arr, vector<vector<int> >&ans, int index)
+    {
+         if(index==arr.size())
+    {
+        ans.push_back(arr);
+        return;
+    }
+    vector<bool>used(21,0);
+    for(int i=index;i<arr.size();i++)
+    {
+        if(used[arr[i]+10]==0)
+        {
+            swap(arr[index],arr[i]);
+            permut(arr,ans,index+1);
+            swap(arr[index],arr[i]);
+            used[arr[i]+10]=1;
+        }
+    }   
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int> >ans;
+        permut(nums,ans,0);
+        return ans;
+    }
+};
