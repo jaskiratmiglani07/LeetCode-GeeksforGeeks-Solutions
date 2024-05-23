9b03beb99abf0fcde3c37872e7006a1db2e83f33
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-05-23 13:28:34 +0530

    Updated Solution - GfG to GitHub

--- Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
+++ Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
@@ -119,37 +119,62 @@ int intersectPoint(Node* head1, Node* head2)
 //intersection point in Y shaped linked list
 //if they do not interesect, return -1
 //count the number of nodes
-Node *curr1 = head1, *curr2 = head2;
-int count1 = 0, count2 = 0;
-while( curr1 )
-{
-    count1++;
-    curr1 = curr1->next;
-}
-while( curr2 )
-{
-    count2++;
-    curr2 = curr2->next;
-}
-curr1 = head1;
-curr2 = head2;
-while(count1 > count2)
-{
-    curr1 = curr1->next;
-    count1--;
-}
-while(count2 > count1)
-{
-    curr2 = curr2->next;
-    count2--;
-} 
-while(curr1 != curr2)
-{
-    curr1 = curr1->next;
-    curr2 = curr2->next;
-}
-if(!curr1)
-return -1;
-return curr1->data;
-}
+// Node *curr1 = head1, *curr2 = head2;
+// int count1 = 0, count2 = 0;
+// while( curr1 )
+// {
+//     count1++;
+//     curr1 = curr1->next;
+// }
+// while( curr2 )
+// {
+//     count2++;
+//     curr2 = curr2->next;
+// }
+// curr1 = head1;
+// curr2 = head2;
+// while(count1 > count2)
+// {
+//     curr1 = curr1->next;
+//     count1--;
+// }
+// while(count2 > count1)
+// {
+//     curr2 = curr2->next;
+//     count2--;
+// } 
+// while(curr1 != curr2)
+// {
+//     curr1 = curr1->next;
+//     curr2 = curr2->next;
+// }
+// if(!curr1)
+// return -1;
+// return curr1->data;
+            //method 2
+            Node *curr1 = head1;
+            while(curr1->next!=nullptr)
+            {
+                curr1 = curr1->next;
+            }
+            curr1->next = head1;
+            Node *slow = head2;
+            Node *fast = head2;
+            while(fast != nullptr && fast->next!= nullptr)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow == fast)
+                break;
+            }
+            if(fast == nullptr || fast->next == nullptr)
+            return -1;
+            slow = head2;
+            while(slow!= fast)
+            { 
+            slow = slow->next;
+            fast = fast->next;
+            }
+            return slow->data;
+            }
 
