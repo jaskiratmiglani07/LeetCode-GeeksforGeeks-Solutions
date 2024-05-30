e9d247e4311c74132bc806c02a306313c30cae6d
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-05-30 12:19:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String Manipulation/string-manipulation.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int removeConsecutiveSame(vector <string > v) 
+    {
+        // Your code goes here
+        stack<string>s;
+    for(int i=0; i<v.size(); i++)
+    {
+        if(s.empty())//empty stack
+        s.push(v[i]);
+        else if(s.top()==v[i])//they same
+        s.pop();
+        else//they different
+        s.push(v[i]);
+    }
+return s.size();
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         string s;
+         vector<string> v;
+         for(int i=0;i<n;i++)
+         {
+             cin>>s;
+             v.push_back(s);
+         }
+         Solution ob;
+         cout<< ob.removeConsecutiveSame(v) <<endl;
+     }
+	
+	
+	
+	
+	return 0;
+}
+// } Driver Code Ends
