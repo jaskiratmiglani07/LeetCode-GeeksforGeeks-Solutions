c6d73627f8159ca259c8df674eb5aa2ee49dfdd7
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-07-02 15:17:12 +0530

    Time: 100 ms (87.05%), Space: 79.7 MB (74.30%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,32 +1,63 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-    stack<int>st;
-    vector<int>right(heights.size(),heights.size());//stores the indices of next smallest elements to the left
-    vector<int>left(heights.size(),-1);//stores the indices of next smallest elements to the right
-    for(int i=0; i<heights.size() ; i++)
-    {
+        //method 1
+//     stack<int>st;
+//     vector<int>right(heights.size(),heights.size());//stores the indices of next smallest elements to the left
+//     vector<int>left(heights.size(),-1);//stores the indices of next smallest elements to the right
+//     for(int i=0; i<heights.size() ; i++)
+//     {
+//     while(!st.empty() && heights[st.top()]>heights[i])
+//     {
+//         right[st.top()]=i;
+//         st.pop();
+//     }
+//     st.push(i);
+//     }
+//     for(int i=(heights.size()-1); i>=0 ; i--)
+//     {
+//     while(!st.empty() && heights[st.top()]>heights[i])
+//     {
+//         left[st.top()]=i;
+//         st.pop();
+//     }
+//     st.push(i);
+//     }
+//     int ans=0;
+//     for(int i=0; i<heights.size(); i++)
+//     {
+//     ans = max(ans, heights[i]*(right[i]-left[i]-1));
+//     }
+// return ans;
+        //method 2
+stack<int>st;
+int index;
+int ans=0;
+int n=heights.size();
+for(int i=0; i<heights.size() ; i++)
+{
     while(!st.empty() && heights[st.top()]>heights[i])
-    {
-        right[st.top()]=i;
+    {   
+        index= st.top();
         st.pop();
+        if(!st.empty())
+        ans=max(ans,heights[index]*(i-st.top()-1));
+        else
+        {
+        ans=max(ans, heights[index]*i);
+        }
     }
     st.push(i);
-    }
-    for(int i=(heights.size()-1); i>=0 ; i--)
-    {
-    while(!st.empty() && heights[st.top()]>heights[i])
-    {
-        left[st.top()]=i;
-        st.pop();
-    }
-    st.push(i);
-    }
-    int ans=0;
-    for(int i=0; i<heights.size(); i++)
-    {
-    ans = max(ans, heights[i]*(right[i]-left[i]-1));
-    }
-return ans;
+}
+        while(!st.empty())
+        {
+            index = st.top();
+            st.pop();
+            if(!st.empty())
+                ans = max(ans, heights[index]*(n-st.top()-1));
+            else
+                ans = max(ans, heights[index]*n);
+        }
+        return ans;
     }
 };
