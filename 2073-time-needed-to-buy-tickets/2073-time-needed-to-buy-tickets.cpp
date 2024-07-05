6db6f442004fdb9b0a4ee23df9072956ee99859e
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2024-07-05 17:58:57 +0530

    Update 2073-time-needed-to-buy-tickets.cpp

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -10,6 +10,8 @@ public:
         while(tickets[k]!=0)
         {
             tickets[q.front()]--;
+//until tickets[q.front()] turns 0, push the index to the back of the queue and then pop the index at the front.
+//when the tickets[q.front()] does turn 0, simply pop it. don't push anything into the queue.
             if(tickets[q.front()])
                 q.push(q.front());
             q.pop();
